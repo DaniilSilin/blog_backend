59869c6f5a485ea6b08e7d4ace90deeb7f52f3ea
unknown <elico161001@gmail.com>
2025-09-20 21:03:10 +0400

    staticfiles issue fix

--- mediasoft_django_hard/settings.py
+++ mediasoft_django_hard/settings.py
@@ -84,6 +84,8 @@ ROOT_URLCONF = 'mediasoft_django_hard.urls'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+
+
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3001",
     "http://127.0.0.1:3000",
@@ -165,7 +167,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Путь к директории, где будут собираться статические файлы
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Убедитесь, что эта папка существует
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
