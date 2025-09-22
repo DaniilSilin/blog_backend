e1bae04bc10b58a0ef9e8e2fe17b5a69683f2cd5
unknown <elico161001@gmail.com>
2025-09-23 02:52:57 +0400

    fix cors 2

--- mediasoft_django_hard/settings.py
+++ mediasoft_django_hard/settings.py
@@ -95,6 +95,7 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3001",
