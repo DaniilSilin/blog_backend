47cd6ff400ecdad0d676bea923e0a865068bab4e
unknown <elico161001@gmail.com>
2025-09-23 17:02:24 +0400

    cors fix

--- mediasoft_django_hard/settings.py
+++ mediasoft_django_hard/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CORS_ALLOW_ALL_ORIGINS = True
 
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = [
     '79.174.92.166',
     '127.0.0.1',
     '0.0.0.0',
+    'localhost',
 ]
 
 # Application definition
@@ -76,11 +77,11 @@ REST_FRAMEWORK = {
 # }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'social_net.middleware.DisableCSRFMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -100,6 +101,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8080",
     "http://localhost:8080",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 TEMPLATES = [
