4c8330b709c20479ddd7843b483794193ac289f9
unknown <elico161001@gmail.com>
2025-09-29 20:53:24 +0400

    added captcha validation

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework.serializers import CharField, ModelSerializer
 from django.core.validators import RegexValidator
-from .validators import validate_first_name, validate_last_name
+from .validators import validate_first_name, validate_last_name, validate_email
 from .models import UserProfile
 from social_net.models import Blog
 from social_net.serializers import BlogSerializer
@@ -27,6 +27,10 @@ class RegisterSerializer(serializers.ModelSerializer):
         validators=[validate_last_name]
     )
 
+    email = CharField(
+        required=True,
+    )
+
     username = CharField(
         required=True,
         min_length=3,
@@ -39,9 +43,13 @@ class RegisterSerializer(serializers.ModelSerializer):
         max_length=50,
     )
 
+    token = CharField(
+        required=True,
+    )
+
     class Meta:
         model = UserProfile
-        fields = ('first_name', 'last_name', 'email', 'username', 'password')
+        fields = ('first_name', 'last_name', 'email', 'username', 'password', 'token')
 
 
 class LoginSerializer(serializers.ModelSerializer):
@@ -71,4 +79,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserProfile
-        fields = '__all__'
+        fields = (
+        'id', 'last_login', 'is_superuser', 'username', 'first_name', 'last_name', 'email', 'is_active', 'date_joined',
+        'is_admin', 'gender', 'description', 'date_of_birth', 'is_profile_private', 'last_activity', 'avatar', 'avatar_small',
+        'banner', 'banner_small')
--- authentication/viewsets.py
+++ authentication/viewsets.py
@@ -1,12 +1,24 @@
-from django.http import HttpResponseRedirect
-from rest_framework.response import Response
+import os
+
+from django.contrib.sites import requests
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status, viewsets, permissions
 from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect
 
+import requests
+
+import sys
+import json
+
 from .models import UserProfile
 from .serializers import LoginSerializer, RegisterSerializer, LogoutSerializer, UserSerializer
+
+
+from dotenv import dotenv_values, load_dotenv
+
+load_dotenv()
+config = dotenv_values(".env")
 
 
 from rest_framework.authtoken.models import Token
@@ -32,7 +44,7 @@ class LoginView(APIView):
 
 class RegisterView(viewsets.ModelViewSet):
     serializer_class = RegisterSerializer
-    permission_classes = (permissions.AllowAny,)
+    permission_classes = [AllowAny]
 
     def create(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
@@ -42,22 +54,38 @@ class RegisterView(viewsets.ModelViewSet):
         email = serializer.data['email']
         username = serializer.data['username']
         password = serializer.data['password']
+        token = serializer.data['token']
 
-        user = UserProfile(
-            first_name=first_name,
-            last_name=last_name,
-            email=email,
-            username=username,
-            is_admin=False
+        if self.check_captcha(token):
+            user = UserProfile(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                is_admin=False
+            )
+            user.set_password(password)
+            user.save()
+            return Response({'status': 'successful'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'status': 'unsuccessful'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def check_captcha(self, token):
+        SMARTCAPTCHA_SERVER_KEY = os.getenv("SMARTCAPTCHA_SERVER_KEY")
+        resp = requests.post(
+            "https://smartcaptcha.yandexcloud.net/validate",
+            data={
+                "secret": SMARTCAPTCHA_SERVER_KEY,
+                "token": token,
+            },
+            timeout=1
         )
-        user.set_password(password)
-        user.save()
-
-        # user = authenticate(username=username, password=password)
-        #
-        # if user:
-        #     token, created = Token.objects.get_or_create(user=user)
-        return Response({'status': 'successful'}, status=status.HTTP_200_OK)
+        server_output = resp.content.decode()
+        if resp.status_code != 200:
+            print(f"Allow access due to an error: code={resp.status_code}; message={server_output}",
+                  file=sys.stderr)
+            return True
+        return json.loads(server_output)["status"] == "ok"
 
 
 class LogoutView(viewsets.ModelViewSet):
